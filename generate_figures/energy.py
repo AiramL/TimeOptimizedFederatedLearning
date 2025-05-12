c437c31b1e1212362859b07524dd99d7596fe1b0
airaml <airam@poli.ufrj.br>
2025-05-12 11:27:14 +0200

    updating experiment 3

---
+++ generate_figures/energy.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+
+from numpy import mean, std
+from pickle import load
+
+from legends import legends_dicts
+
+def plot_energy(dictionary, 
+                PLOT=False, 
+                language="pt"):
+    
+    plt.figure(figsize=(12, 8))
+
+    x_axis = dictionary[list(dictionary.keys())[0]].keys()
+
+    legends = legends_dicts[language]
+
+    for server in dictionary.keys():
+        
+        means = []
+        stds = []
+        
+        for err in dictionary[server].keys():
+            
+            means.append(mean(dictionary[server][err],axis=0))
+            stds.append(std(dictionary[server][err],axis=0))
+            
+            
+        plt.errorbar(x_axis, means, yerr=stds, capsize=3, label=legends[server])
+
+    if language == "en":        
+
+        plt.xlabel("Clients' Error Rate (%)")
+        plt.ylabel("Training Efficiency (%)")
+
+    elif language == "pt":
+        
+        plt.xlabel("Taxa de Erro por Cliente (%)")
+        plt.ylabel("Eficiência de Treinamento (%)")
+    
+    plt.legend()
+
+    plt.savefig("figures/training_efficiency_"+language+".png",
+                dpi=300,
+                bbox_inches='tight')
+
+    if PLOT:
+        plt.show()
+
+
+if __name__ == "__main__":
+
+    with open("results/energy","rb") as reader:
+        dictionary = load(reader)
+
+    plot_energy(dictionary, 
+                PLOT=True)
---
+++ generate_figures/legends.py
@@ -0,0 +1,18 @@
+
+legends_dicts = { "en": {"random": "Random",
+                         "m_fastest": "M-Fastest (M=50%)",
+                         "tofl_oracle": "TOFL Oracle",
+                         "tofl_estimator_dl" : "TOFL Estimator",
+                         "tofl": "TOFL Estimator",
+                         "tofl_estimator_m_fastest": "TOFL with M-Fastest",
+                         "tofl_mfastest": "TOFL with M-Fastest"},
+
+                "pt":   {"random": "Aleatório",
+                         "m_fastest": "M-Fastest (M=50%)",
+                         "tofl_oracle": "TOFL Oráculo",
+                         "tofl_estimator_dl" : "TOFL Estimador",
+                         "tofl": "TOFL Estimador",
+                         "tofl_estimator_m_fastest": "TOFL com M-Fastest",
+                         "tofl_mfastest": "TOFL com M-Fastest"} 
+                         
+                         }
