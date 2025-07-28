05781509a17a00319abc17da50cfdef7dd441e44
AiramL <airam@poli.ufrj.br>
2025-07-28 22:18:20 +0000

    final version of generate_time2acc

--- generate_figures/generate_time2acc.py
+++ generate_figures/generate_time2acc.py
@@ -2,81 +2,97 @@ from pickle import load
 import matplotlib.pyplot as plt
 from itertools import accumulate
 
-def process_accuracy_delays(n_clients=95,
-                            dataset="WiSec",
-                            acc_path="results/classification/processed/",
-                            time_path="results/client_selection/processed/",
-                            n_executions=10,
-                            model_size=500,
-                            language="pt"):
+from legends import legends_dicts
+
+def process_accuracy_delays(n_clients=95,                                                                               dataset="WiSec",                                                                            acc_path="results/classification/processed/",                                               time_path="results/client_selection/processed/",                                            n_executions=10,                                                                            model_size=500,                                                                             language="pt"):
+
     
-    plt.figure(figsize=(12, 8))
-    
+    plt.figure(figsize=(12, 8)) 
+
     servers = ["random",
                "m_fastest",
                "tofl_oracle",
                "tofl_estimator_dl",
                "tofl_estimator_m_fastest"]
-   
-    
+
+    legends = legends_dicts[language]
+
     if language == "en":
-        legends = {"random": "Random",
-                   "m_fastest": "M-Fastest (M=50%)",
-                   "tofl_oracle": "TOFL Oracle",
-                   "tofl_estimator_dl" : "TOFL Estimator",
-                   "tofl_estimator_m_fastest": "TOFL Estimating and Selecting M-Fastest Clients"}
-        
-        plt.xlabel("Time (s)")
-        plt.ylabel("Accuracy (%)")
 
-    elif language == "pt":
-        legends = {"random": "Aleatório",
-                   "m_fastest": "M-Fastest (M=50%)",
-                   "tofl_oracle": "TOFL Oráculo",
-                   "tofl_estimator_dl" : "TOFL Estimador",
-                   "tofl_estimator_m_fastest": "TOFL Estimando e Selecionando os M-Fastest Clientes"}
-        
-        plt.xlabel("Tempo (s)")
-        plt.ylabel("Acurácia (%)")
+        plt.xlabel("Time (s)", fontsize=16)
 
+        plt.ylabel("Accuracy (%)", fontsize=16)
+
+    elif language == "pt":  
+
+        plt.xlabel("Tempo (s)", fontsize=16) 
+
+        plt.ylabel("Acurácia (%)", fontsize=16) 
 
     results_time = { }
+
     results = { }
-    
+
+
     for server in servers:
+
         with open(time_path+"server_"+server+"_n_clients_selected_"+str(n_clients)+"_mean","rb") as loader:
+
             result_list = load(loader)
             results_time[server] =  list(accumulate(result_list))
 
     for server in servers:
-            
+
         if server == "m_fastest" or server == "tofl_estimator_m_fastest":
             mean_file = acc_path+"m_fastest/"+dataset+"_mean_model"
             std_file = acc_path+"m_fastest/"+dataset+"_std_model"
-            
+
         else:
             mean_file = acc_path+"random/"+dataset+"_mean_model"
             std_file = acc_path+"random/"+dataset+"_std_model"
-        
+
         with open(mean_file,"rb") as loader:
             result_list = load(loader)
             results[server+"mean"] = result_list*100
-        
+
         with open(std_file,"rb") as loader:
             result_list = load(loader)
             results[server+"std"] = result_list*100
-    
+
     for server in servers:
-        plt.errorbar(results_time[server][1:], 
-                     results[server+"mean"][:40], 
-                     yerr=results[server+"std"][:40], 
-                     capsize=3, 
+        plt.errorbar(results_time[server][1:],
+                     results[server+"mean"][:40],
+                     yerr=results[server+"std"][:40],
+                     capsize=3,
                      label=legends[server])
-    
-    plt.legend()
-    plt.savefig("figures/"+dataset+"_time2acc.png",dpi=300,bbox_inches='tight')
+
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
+    plt.legend(fontsize=20)
+    plt.savefig("figures/"+dataset+"_time2acc_n_clients_"+str(n_clients)+language+".png",dpi=300,bbox_inches='tight')
+
 
 if __name__ == "__main__":
 
-    process_accuracy_delays(dataset="WiSec")
-    process_accuracy_delays(dataset="VeReMi")
+    n_clients = [16, 95]
+    languages = ["en", "pt"]
+    datasets = ["WiSec", "VeReMi"]
+
+    for client in n_clients:
+
+        for lang in languages:
+
+            for dataset in datasets:
+
+                process_accuracy_delays(dataset=dataset,
+                                        n_clients=client,
+                                        language=lang)
+
+                                                               
+
+
+
+
+
+
+
