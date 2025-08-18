0d3bb69d606691c5d0a09fbd25dadfe86a448a86
AiramL <airam@poli.ufrj.br>
2025-08-18 16:07:00 -0300

    updating readme and dependencies

--- scripts/build/dependencies.sh
+++ scripts/build/dependencies.sh
@@ -1,5 +1,12 @@
-echo "Installing SUMO"
+#!/bin/bash
 
+echo "Installing conda"
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh
+source ~/.bashrc
+
+echo "Installing SUMO"
 sudo add-apt-repository ppa:sumo/stable
 sudo apt-get update
 sudo apt-get install sumo sumo-tools sumo-doc virtualbox -y
