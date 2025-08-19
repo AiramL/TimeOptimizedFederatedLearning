4e15954e3cf0901df1acedd01a544edd18328c9b
Lucas Airam Castro de Souza <33198508+AiramL@users.noreply.github.com>
2025-08-19 11:52:39 -0300

    Update baremetal.sh

--- scripts/run/baremetal.sh
+++ scripts/run/baremetal.sh
@@ -17,9 +17,9 @@ echo "Starting server"
 if [ "$strategy" = "m_fastest" ]
 then	
 	[[ $(($numClientsFit/2)) = 0 ]] && numClientsFit=1 || numClientsFit=$(($numClientsFit/2))
-	python3.12 src/federated_learning/server.py -ncf=$numClientsFit -tf=$TOFL -nc=$numClients -nor=$eps &
+	python3.12 -m src.federated_learning.server.py -ncf=$numClientsFit -tf=$TOFL -nc=$numClients -nor=$eps &
 else	
-	python3.12 src/federated_learning/server.py -ncf=$numClientsFit -tf=$TOFL -nc=$numClients -nor=$eps &
+	python3.12 -m src.federated_learning.server.py -ncf=$numClientsFit -tf=$TOFL -nc=$numClients -nor=$eps &
 fi
 
 echo "Starting clients"
@@ -30,10 +30,10 @@ for i in $(seq $numClients)
 do
 	if [ $TOFL -eq 1 ]
 	then
-		python3.12 src/federated_learning/client.py -cid=$i -b=$bs -cf=0 >> results/classification/tofl/client_"$i" &
+		python3.12 -m src.federated_learning.client.py -cid=$i -b=$bs -cf=0 >> results/classification/tofl/client_"$i" &
 	
 	else
-		python3.12 src/federated_learning/client.py -nc=$numClients -cid=$i -b=$bs -cf=0 -ncf=$numClientsFit >> results/classification/raw/$strategy/$dataset/"client_""$i" &
+		python3.12 -m src.federated_learning.client.py -nc=$numClients -cid=$i -b=$bs -cf=0 -ncf=$numClientsFit >> results/classification/raw/$strategy/$dataset/"client_""$i" &
 	fi
 	echo "Waiting client "$i" initialization"
 	sleep 2
